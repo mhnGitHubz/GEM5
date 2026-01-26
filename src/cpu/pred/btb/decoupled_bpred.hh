8522ebbe2dc00f8c3b4293629620971fa050c700
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2026-01-26 17:20:31 +0800

    cpu-o3: ideal 2-taken implementation
    Change-Id: I39d54a0621d139cc00a156b02a6d7d888d9b15f0
    Co-authored-by: Xu Boran <xuboran@bosc.ac.cn>

--- src/cpu/pred/btb/decoupled_bpred.hh
+++ src/cpu/pred/btb/decoupled_bpred.hh
@@ -145,6 +145,8 @@ class DecoupledBPUWithBTB : public BPredUnit
 
     unsigned numOverrideBubbles{0};
 
+    bool enableTwoTaken{true};
+
     bool validateFSQEnqueue();
 
     void processNewPrediction();
